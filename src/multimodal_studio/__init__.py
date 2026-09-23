a03e4c7799cc7d14723d3d586e48b43666174537
Maharshi Patel <pmaharshi999@gmail.com>
2026-09-23 02:38:51 +0200

    feat(media): export ffmpeg executor

--- src/multimodal_studio/__init__.py
+++ src/multimodal_studio/__init__.py
@@ -1,3 +1,13 @@
+from .executor import FFmpegExecutor, MediaExecutionError, MediaProbe, probe_media
 from .planner import EditNode, plan, to_json, validate
 
-__all__ = ["EditNode", "plan", "to_json", "validate"]
+__all__ = [
+    "EditNode",
+    "FFmpegExecutor",
+    "MediaExecutionError",
+    "MediaProbe",
+    "plan",
+    "probe_media",
+    "to_json",
+    "validate",
+]
